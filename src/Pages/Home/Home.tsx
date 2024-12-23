dabfca90fe477fca9af6c4c39ace8e07e73b82dd
laptop_Maksim_Alekseev <maxim040597@gmail.com>
2024-12-24 00:22:50 +0300

    add block button_cart

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,11 +4,13 @@ import Header from "../../UI/Header/Header";
 import NewProducts from "../../UI/NewProducts/NewProducts";
 import Products from "../../UI/Products/Products";
 import Footer from "../../UI/Footer/Footer";
+import CartBtn from "../../UI/CartBtn/CartBtn";
 
 
 export default function Home(){
     return(
         <div className={style.home}>
+            <CartBtn/>
             <Header />
             <NewProducts />
             <Products/>
--- src/UI/Card/Card.tsx
+++ src/UI/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './Card.module.sass';
 
 type Product={
@@ -7,26 +7,159 @@ type Product={
         title: string;
         link: string;
         description: string;
-        price_zz: number;
-        price_ss: number;
+        price_zz: number | number[];
+        price_ss: number | number[];
         compound: string;
-        weight: number;
-        image: string;
-        proteins: number;
-        fats: number; 
-        carbohydrates: number;
+        weight: number | number[];
+        image: string | string[];
+        proteins?: number;
+        fats?: number; 
+        carbohydrates?: number;
         icons: number[];
         status: number; 
         category: number;
+        sub_category?: number;
         queue: number;
     };
 
 type propsCard = {
     products: Product[],
     category: number,
+    sub_category: number,
 };
 
-export default function Card(props: propsCard){
+export function Card(props: propsCard){
+    let products = props.products;
+    let category = props.category;
+    let sub_category = props.sub_category;
+    
+    let productCard = products.map(product=>
+        (product.category==category && product.status==1 && product.sub_category==sub_category)?
+        <div className={style.card} key={product.id}>
+            <div className={style.card__wrap}>
+                <div className={style.card__img__wrap}>
+                    <img className={style.card__img} src={`images/${product.image}`} alt={product.title} />    
+                </div>
+                <div className={style.card__textTop}>
+                    <h3 className={style.card__title}>{product.title}</h3>
+                    <span className={style.card__price}> {(product.price_zz)? product.price_zz: 'NaN'} </span>
+                </div>
+                <div className={style.card__text}>
+                    <div className={style.card__text__left}>
+                        {product.description}
+                    </div>
+                    <div className={style.card__text__right}>
+                        <p>{product.weight}г</p>
+                        <button className={style.card__btn}>+</button>
+                    </div>
+                </div>    
+            </div>
+        </div>
+        :
+        ''
+    )
+    return(
+    <>
+        {productCard}
+    </>
+    );
+}
+
+export function CardSet(props: propsCard){
+    let products = props.products
+    let category = props.category
+    let productCard = products.map(product=>
+        (product.category==category && product.status==1)?
+        <div className={style.card+" "+style.card__sets} key={product.id}>
+            <div className={style.card__wrap}>
+                <div className={style.card__img__wrap}>
+                    <img className={style.card__img} src={`images/${product.image}`} alt={product.title} />    
+                </div>
+                <div className={style.card__textTop}>
+                    <h3 className={style.card__title}>{product.title}</h3>
+                    <span className={style.card__price}> {(product.price_zz)? product.price_zz: 'NaN'} </span>
+                </div>
+                <div className={style.card__text}>
+                    <div className={style.card__text__left}>
+                        {product.description}
+                    </div>
+                    <div className={style.card__text__right}>
+                        <p>Вес: {product.weight}г</p>
+                        <button className={style.card__btn}>+</button>
+                    </div>
+                </div>    
+            </div>
+        </div>
+        :
+        ''
+    )
+    return(
+    <>
+        {productCard}
+    </>
+    );
+}
+
+export function CardDich(props: propsCard){
+    let products = props.products;
+    let category = props.category;
+    let sub_category = props.sub_category;
+
+    let [count, SetCount] = useState(0);
+    function Count(){
+        if(count==1){
+            SetCount(0)
+        }
+        else{
+            SetCount(1)
+        }
+    }
+    let productCard = products.map(product=>
+        (product.category==category && product.status==1 && product.sub_category==sub_category)?
+        <div className={style.card} key={product.id}>
+            <div className={style.card__wrap}>
+                <div className={style.card__img__wrap}>
+                    <img className={style.card__img} src={(Array.isArray(product.image))?`images/${product.image[count]}`:`images/${product.image}`} alt={product.title} />    
+                </div>
+                <div className={style.card__textTop}>
+                    <h3 className={style.card__title}>{product.title}</h3>
+                    <span className={style.card__price}> {(Array.isArray(product.price_zz))? product.price_zz[count]:product.price_zz} </span>
+                </div>
+                <div className={style.card__text}>
+                    <div className={style.card__text__left}>
+                        {product.description}
+                        {
+                            (Array.isArray(product.price_zz))?
+                            <>
+                                <label htmlFor="btnCount">
+                                    <input type="checkbox" name={`count${product.id}`} id="btnCount" onClick={Count}/>
+                                    {(Array.isArray(product.weight))? `${product.weight[count]}`:``}
+                                </label>
+                                
+                            </>
+                            :
+                            ''
+                        }
+                    </div>
+                    <div className={style.card__text__right}>
+                        <p>{(Array.isArray(product.weight))? `${product.weight[count]} шт`:` ${product.weight} г`}</p>
+                        
+                        <button className={style.card__btn}>+</button>
+                    </div>
+                </div>    
+            </div>
+        </div>
+        :
+        ''
+    )
+    return(
+    <>
+        {productCard}
+    </>
+    );
+}
+
+export function CardDrink(props: propsCard){
     let products = props.products
     let category = props.category
     let productCard = products.map(product=>
@@ -42,10 +175,10 @@ export default function Card(props: propsCard){
                 </div>
                 <div className={style.card__text}>
                     <div className={style.card__text__left}>
-                        Состав:<br/>{product.compound}
+                        {product.description}
                     </div>
                     <div className={style.card__text__right}>
-                        <p>Вес: {product.weight}г</p>
+                        <p>{product.weight}л</p>
                         <button className={style.card__btn}>+</button>
                     </div>
                 </div>    
